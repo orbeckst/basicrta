4a4cffb2b1b709bd0a5f6eee0af498a2fc7c5dac
Ricky Sexton <rsexton2@asu.edu>
2022-03-11 19:35:15 -0700

    changed name gibbs_mp

---
+++ scripts/gibbs_mp.py
@@ -0,0 +1,51 @@
+from basicrta import *
+from multiprocessing import Pool, Lock
+from basicrta import istarmap
+
+
+if __name__ == "__main__":
+    # Parts of code taken from Shep (Centrifuge3.py, SuperMCMC.py)
+
+     import argparse
+     parser = argparse.ArgumentParser()
+     parser.add_argument('--contacts')
+     args = parser.parse_args()
+     a = np.load(args.contacts)
+#    a = np.load('lipswap_contacts_7.0.npy')
+
+    nproc = 8
+    u = mda.Universe('step7_fixed.pdb', 'fixrot_dimer.xtc')
+    # u = mda.Universe('../step7_fixed.pdb', '../fixrot_dimer.xtc')
+    ids = u.select_atoms('protein').residues.resids
+    names = u.select_atoms('protein').residues.resnames
+    names = np.array([mda.lib.util.convert_aa_code(name) for name in names])
+    resids, resnames = ids[np.unique(a[:, 0]).astype(int)], names[np.unique(a[:, 0]).astype(int)]
+    residues = np.array([f'{resnames[i]}{resids[i]}' for i in range(len(resids))])
+    times = np.array([a[a[:, 0] == i][:, 3] for i in range(len(residues))], dtype=object)
+    trajtimes = np.array([a[a[:, 0] == i][:, 2] for i in range(len(residues))], dtype=object)
+
+    if not os.path.exists('BaSiC-RTA'):
+        os.mkdir('BaSiC-RTA')
+    os.chdir('BaSiC-RTA')
+
+    input_list = np.array([[residues[i], times[i]] for i in range(len(residues))], dtype=object)
+    with Pool(nproc, initializer=tqdm.set_lock, initargs=(Lock(),)) as p:
+        for _ in tqdm(p.istarmap(run_residue, input_list), total=len(residues), position=0, desc='overall progress'):
+            pass
+
+    rem_inds = get_remaining_residue_inds(residues)
+    times, trajtimes = times[rem_inds], trajtimes[rem_inds]
+
+    residues, t_slow, sd, indicators = collect_results()
+    plot_protein(residues, t_slow, sd)
+    check_results(residues, times)
+    plot_hists(times, indicators, residues)
+
+    # print(residues[0])
+    # input_list = np.array([[u, times[i], trajtimes[i], indicators[i], residues[i]] for i in range(len(residues))],
+    #                       dtype=object)
+    # input_list = np.array([[u, times[i], trajtimes[i], indicators[i], residues[i]] for i in range(1)],
+    #                       dtype=object)
+    # Pool(nproc, initializer=tqdm.set_lock, initargs=(Lock(),)).starmap(write_trajs, input_list)
+
+    # write_trajs(u, times, trajtimes, indicators, residues)
