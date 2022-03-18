e8d1d4560ce86e2171be381772a83833b3d48714
Ricky Sexton <rsexton2@asu.edu>
2022-03-18 12:36:09 -0700

    clean up conflicts

--- basicrta/functions.py
+++ basicrta/functions.py
@@ -375,22 +375,17 @@ def get_start_stop_frames(simtime, timelen):
     return frame, frame+framec
 
 
-#def write_trajs(u, time, trajtime, indicator, residue, lipind, i):
-def write_trajs(inlist):
-    u, time, trajtime, indicator, residue, lipind, i = inlist
-    print(f'universe: {u}')
+def write_trajs(u, time, trajtime, indicator, residue, lipind):
     try:
         proc = int(multiprocessing.current_process().name[-1])
     except ValueError:
         proc = 1
-    print(f'process: {proc}')
+
     prot, chol = u.select_atoms('protein'), u.select_atoms('resname CHOL')
     inds = np.array([np.where(indicator.argmax(axis=0) == i)[0] for i in range(8)], dtype=object)
     lens = np.array([len(ind) for ind in inds])
     ncomps = len(np.where(lens != 0)[0])
-    print(f'parsed')
     for comp in range(ncomps):
-        print(f'comp {comp}')
         bframes, eframes = get_start_stop_frames(trajtime[inds[comp]], time[inds[comp]])
         sortinds = bframes.argsort()
         bframes.sort()
@@ -398,15 +393,10 @@ def write_trajs(inlist):
         tmp = [np.arange(b, e) for b, e in zip(bframes, eframes)]
         tmpL = [np.ones_like(np.arange(b, e))*l for b, e, l in zip(bframes, eframes, lipind[inds[comp]])]
         write_frames, write_Linds = np.concatenate([*tmp]), np.concatenate([*tmpL]).astype(int)
-        print(f'writing')
-#        with mda.Writer(f"{residue}/comp{comp}_traj.xtc", len((prot+chol.residues[0].atoms).atoms)) as W:
-#            for i, ts in tqdm(enumerate(u.trajectory[write_frames]), desc=f"{residue}-comp{comp}", position=proc,
-#                              leave=False, total=len(write_frames)):
-#                W.write(prot+chol.residues[write_Linds[i]].atoms)
-        for i, ts in tqdm(enumerate(u.trajectory[write_frames]), desc=f"{residue}-comp{comp}", position=proc,
-                          leave=False, total=len(write_frames)):
-            print(prot+chol.residues[write_Linds[i]].atoms)
-            print('printing')
+        with mda.Writer(f"{residue}/comp{comp}_traj.xtc", len((prot+chol.residues[0].atoms).atoms)) as W:
+            for i, ts in tqdm(enumerate(u.trajectory[write_frames]), desc=f"{residue}-comp{comp}", position=proc,
+                              leave=False, total=len(write_frames)):
+                W.write(prot+chol.residues[write_Linds[i]].atoms)
 
 
 def plot_hists(timelens, indicators, residues):
