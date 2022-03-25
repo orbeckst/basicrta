530349e19019b1deb172c1d683c23c3c83ed071e
Ricky Sexton <rsexton2@asu.edu>
2022-03-25 09:56:28 -0700

    testing input

--- scripts/replot.py
+++ scripts/replot.py
@@ -4,8 +4,8 @@ from basicrta.functions import collect_n_plot
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument('--resids', nargs='+')
-    parser.add_argument('--comps', nargs='*')
+    parser.add_argument('--resids', nargs='+', type=int)
+    parser.add_argument('--comps', nargs='*', type=int)
     args = parser.parse_args()
     resids, comps = args.resids, args.comps
     print(resids, comps)
