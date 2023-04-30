eac11d7b074ef1ab6dd72c8a3105e03d394cbccf
Ricky Sexton <rsexton2@asu.edu>
2023-04-29 19:31:00 -0700

    minor fix

--- scripts/replot_protein.py
+++ scripts/replot_protein.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('--prot')
-    args = parser.parser_args()
+    args = parser.parse_args()
 
     prot = args.prot
     residues, t_slow, sd, indicators = collect_results()
