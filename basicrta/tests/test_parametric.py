976fb12809f7db961d31bb5827feae9358d7165d
Rick Sexton <rsexton2@asu.edu>
2024-01-04 21:54:31 -0700

    modified parametric test to use rate switching

--- basicrta/tests/test_parametric.py
+++ basicrta/tests/test_parametric.py
@@ -27,7 +27,7 @@ def test_parametric():
     tmp = np.array([np.sort(G.results.rates[:,i]) for i in range(G.results.ncomp)])
     tmp2 = (tmp.cumsum(axis=1).T/tmp.cumsum(axis=1).T[-1])
     tmp3 = tmp.T[[np.where((tmp2[:,i]>0.025)&(tmp2[:,i]<0.975))[0] for i in range(G.results.ncomp)][0]]
-    descsort = np.median(G.results.mcrates, axis=0).argsort()[::-1]
+    descsort = np.median(G.results.mcrates[1000:], axis=0).argsort()[::-1]
     ci = np.array([[line[0],line[-1]] for line in tmp3.T])
 
     Bools = np.array([(rts[i]>ci[descsort][i,0])&(rts[i]<ci[descsort][i,1]) for i in descsort])
