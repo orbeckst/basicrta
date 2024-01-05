517aefb118393f6fe58d4ae7b9aee41e9bfe42ab
Rick Sexton <rsexton2@asu.edu>
2024-01-04 19:52:15 -0700

    modified test_parametric

--- basicrta/tests/test_parametric.py
+++ basicrta/tests/test_parametric.py
@@ -6,7 +6,7 @@ def test_parametric():
     wts = np.array([0.89, 0.098, 0.008, 0.002, 0.00056])
     wts = wts/wts.sum()
     x = simulate_hn(5e4, wts, [4.7, 0.8, 0.2, 0.02, 0.003])
-    G = newgibbs(x, 'X1', 0, 0.1, ncomp=5, niter=25000, sort=False)
+    G = newgibbs(x, 'X1', 0, 0.1, ncomp=5, niter=20000, sort=False)
     G.run()
     tmp = np.array([np.sort(G.results.weights[:,i]) for i in range(G.results.ncomp)])
     tmp2 = (tmp.cumsum(axis=1).T/tmp.cumsum(axis=1).T[-1])
@@ -15,6 +15,9 @@ def test_parametric():
     ci = np.array([[line[0],line[-1]] for line in tmp3.T])
 
 
-    Bools = [(wts[i]>ci[descsort][i,0])&(wts[i]<ci[descsort][i,1]) for i in descsort]
+    Bools = np.array([(wts[i]>ci[descsort][i,0])&(wts[i]<ci[descsort][i,1]) for i in descsort])
+
     assert Bools.all() == True
 
+if __name__=="__main__":
+    test_parametric()
