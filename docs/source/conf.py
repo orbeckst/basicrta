3445b8bc78341575165156ad13fb52282e5b3c7f
Rick Sexton <rsexton2@asu.edu>
2024-01-28 18:54:27 -0700

    modified conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,3 +1,11 @@
+import os
+
+on_rtd = os.environ.get("READTHEDOCS") == "True"
+if on_rtd:
+    html_theme = "default"
+else:
+    html_theme = "nature"
+
 extensions = [
    'sphinx.ext.duration',
    'sphinx.ext.doctest',
