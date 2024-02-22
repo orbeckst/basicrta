08fb56d4e0b01ddc1903de6eaf7f9a02f61eeb1c
Rick Sexton <rsexton2@asu.edu>
2024-02-21 22:07:13 -0700

    modified main paths

--- basicrta/gibbs.py
+++ basicrta/gibbs.py
@@ -382,6 +382,7 @@ if __name__ == '__main__':
     parser.add_argument('--resid', type=int)
     args = parser.parse_args()
 
+    contact_path = os.path.abspath(args.contacts)
     cutoff = args.contacts.split('/')[-1].strip('.pkl').split('_')[-1]
     os.chdir(f'basicrta-{cutoff}')
-    ParallelGibbs(os.path.abspath(args.contacts)).run(run_resids=args.resid)
+    ParallelGibbs(contact_path).run(run_resids=args.resid)
