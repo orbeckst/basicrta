31c0c9cf28e338db0d743ee62a370545cae226d0
Rick Sexton <rsexton2@asu.edu>
2024-03-01 22:09:01 -0700

    fixing local pickle error

--- basicrta/gibbs.py
+++ basicrta/gibbs.py
@@ -52,10 +52,9 @@ class ProcessProtein(object):
         dirs = np.array(glob('?[0-9]*'))
         sorted_inds = np.array([int(adir[1:]) for adir in dirs]).argsort()
         dirs = dirs[sorted_inds]
-        print(len(dirs))
         with Pool(nproc, initializer=tqdm.set_lock, initargs=(Lock(),)) as p:
             try:
-                for _ in tqdm(p.imap(self._collect_res, dirs),
+                for _ in tqdm(p.istarmap(self._collect_res, dirs),
                               total=len(dirs), position=0,
                               desc='overall progress'):
                     pass
